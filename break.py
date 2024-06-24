59b8aa61fa0ffc716f3173312e3b72ce94342236
harani2002 <mharani602@gmail.com>
2024-06-24 12:53:56 +0530

    Add files via upload

---
+++ break.py
@@ -0,0 +1,22 @@
+'''i=0
+while i<=10:
+    i+=1
+    print(i)
+    if i==5:
+        break'''
+
+
+i=0
+while i<=10:
+    i+=1
+    if i==5:
+        continue
+    print(i)
+
+
+i=0
+while i<=10:
+    i+=1
+    if i==5:
+        pass
+    print(i)
